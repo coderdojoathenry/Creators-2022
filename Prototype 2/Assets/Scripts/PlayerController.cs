f08c8f4d81326f983eb3bd95c5bfbe4083b946e8
KieranCoughlan <KieranCoughlan@gmail.com>
2022-11-27 13:29:19 +0000

    Week 7

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float xRange = 20.0f;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -38,5 +39,15 @@ public class PlayerController : MonoBehaviour
                                              transform.position.z);
 
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab,
+                        transform.position,
+                        transform.rotation);
+        }
+
+
     }
 }
