6eceebf08f1eac59c5478405127aae9f764db9b1
KieranCoughlan <KieranCoughlan@gmail.com>
2023-03-25 10:37:11 +0000

    Missing code from week 16

--- Gem Search/Assets/Scripts/PlayerDig.cs
+++ Gem Search/Assets/Scripts/PlayerDig.cs
@@ -6,6 +6,10 @@ public class PlayerDig : MonoBehaviour
 {
     public string GroundLayer = "Ground";
     public GameObject DiggingPrefab;
+    public ItemScatterer ItemScatterer;
+    public float MaxDistToItem = 0.35f;
+    public GameObject GemFoundPrefab;
+    public Transform GemFoundLocation;
 
     public void OnDig()
     {
@@ -20,7 +24,32 @@ public class PlayerDig : MonoBehaviour
         {
             Instantiate(DiggingPrefab, hitInfo.point,
                         DiggingPrefab.transform.rotation);
+            Invoke("DigResult", 3);
         }
+    }
 
+    private void DigResult()
+    {
+        float distToNearest = (ItemScatterer.nearestItem.transform.position -
+                               transform.position).magnitude;
+
+        if (distToNearest > MaxDistToItem)
+            return;
+
+        // Capture the gem definition
+        RandomGem rg = ItemScatterer.nearestItem.GetComponent<RandomGem>();
+        GemDefinition gd = rg.GemDefinition;
+
+        // Remove the item from ItemScatterer
+        Destroy(ItemScatterer.nearestItem);
+        ItemScatterer.nearestItem = null;
+
+        // TODO: Add to our inventory
+
+        // Spawn the Gem Found Prefab to let the player know what they found
+        GameObject gfb = Instantiate(GemFoundPrefab, GemFoundLocation);
+        GemFoundMessage gfm = gfb.GetComponent<GemFoundMessage>();
+        GameObject gem = Instantiate(gd.Prefab, gfm.GemHolder);
+        gfm.SetGemDefinition(gd);
     }
 }
